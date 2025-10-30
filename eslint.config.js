77fc690c8034f989895baf73aad31644d403be63
baddiehustleai-star <baddiehustleai@gmail.com>
2025-10-30 10:37:16 +0000

    chore: add ESLint (flat config), Prettier, Vitest; run lint+test in CI

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// ESLint Flat Config for React + Vite project
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+];

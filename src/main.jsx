f4930e70be3bb3ff149cc42f874f57a3a57641ec
baddiehustleai-star <baddiehustleai@gmail.com>
2025-11-14 08:24:46 -0800

    Fix rendering of Landing component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import Landing from './pages/Landing.jsx';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Landing />

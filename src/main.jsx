3ee99a610cfa5423aed1ac0dc1bcdddd852577c0
baddiehustleai-star <baddiehustleai@gmail.com>
2025-11-14 08:23:51 -0800

    Remove Analytics import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
+
 import './index.css';
 import Landing from './pages/Landing.jsx';
 
